aef70712f0702fef5ff2db7ea26f18c8ca4b1100
Shawn Aviles <shawnaviles26@gmail.com>
2022-12-01 18:46:00 -0500

    day 10

---
+++ classwork/day10/floatingptarrays.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+	double x[10];
+	double a = 2.5;
+	for (int i = 0; i < 10; i ++) 
+		x[i] += a;
+
+	float sum = 0;
+	for (int i = 0; i < 10; i++)
+		sum += x[i];
+}
+
+//                rcx      edx
+double sum(double x[], int n) {
+  float sum = 0;
+	for (int i = 0 ; i < n; i++) {
+		sum += x[i];
+	}
+	return sum;
+}
+/*
+	xmm0.. xmm15  // each one can hold TWO double
+LOOP:
+	RCX = points to start of array
+	pxor	%xmm0, %xmm0         // xmm0 = 0
+	leaq	(%rcx,%rdx,8), %rax  // rax = END OF ARRAY
+	addsd	(%rcx), %xmm0        // xmm0 = xmm0 + a[0]
+	addq	$8, %rcx             // now rcx points to next element
+	cmpq	%rcx, %rax
+	jne	LOOP
+	ret
+	vectorized instructions can do
+	   2 at a time using xmm      AVX
+	   4 at a time using ymm      AVX2
+	   8 at a time using zmm      AVX512
+//  addvd %zmm0, %zmm2 // add 8 double precision numbers
+	
+*/
+
