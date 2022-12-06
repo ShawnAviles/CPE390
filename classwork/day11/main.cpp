05e556a0afb328bd02615e806b8c6f16afa8a431
Shawn Aviles <shawnaviles26@gmail.com>
2022-12-06 15:57:40 -0500

    day 11 start

---
+++ classwork/day11/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+double f(double a, double b);
+double sum(double arr[], int len);
+
+
+main () {
+	cout << f(3.0, 4.0); // (a+b)*(a-b)
+
+	double x[] = {1, 2, 3, 5.5};	// 5.5 = 101.1 (binary)
+
+	cout << sum(x, 4) << '\n';
+
+	// cout << mean(x,4) << endl;
+}
---
+++ classwork/day11/testingfloatingpoint.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double f1 (double a, double b) {
+	return a + b;
+}
+// 	addsd	%xmm1, %xmm0
+
+double f2 (double a, double b) {
+	return a - b;
+}
+// 	subsd	%xmm1, %xmm0
+
+
+double f3 (double a, double b) {
+	return a * b;
+}
+// 	mulsd	%xmm1, %xmm0
+
+double f4 (double a, double b) {
+	return a / b;
+}
+// 	divsd	%xmm1, %xmm0
+
+// 	sqrtsd	%xmm1, %xmm0
+
+
+double f5 (double a) {
+	return -a;
+}
+// xorpd 	LC0(%rip), %xmm0 	#since its a float you just flip the sign bit
+
+double f6 (double a) {
+	return abs(a);
+}
+// andpd	.LC1(%rip), %xmm0 	#and sign bit with 0
+
+double f7 (double a) {
+	return sqrt(a);
+}
